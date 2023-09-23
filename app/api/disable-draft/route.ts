e6ed65b514b20e7144f5a96fb5793df7671ab409
Alexei Gorobet <alexei.gorobet@ffwagency.com>
2023-09-23 01:55:05 -0600

    fix: Please TS by giving it what it asks

--- app/api/disable-draft/route.ts
+++ app/api/disable-draft/route.ts
@@ -3,4 +3,6 @@ import { redirect } from "next/navigation";
 export async function GET(request: Request) {
   draftMode().disable();
   redirect("/");
+  // Trick TypeScript into thinking we're returning a Response, even if redirect will throw an exception.
+  return new Response(null, { status: 307 });
 }
