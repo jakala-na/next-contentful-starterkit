ad1a317bfded1ec404a46058b8aea792a960b9b7
Alexei Gorobet <asgorobets@gmail.com>
2024-04-18 10:02:43 -0400

    feat(forms): fix gql.tada fragment

--- components/form-ctf/from-ctf-client.tsx
+++ components/form-ctf/from-ctf-client.tsx
@@ -1,16 +1,17 @@
 'use client';
 
-import { ComponentFormFieldsFragment } from '#/gql/graphql';
 import { formsMap } from '../forms/forms-mapping';
 import { useComponentPreview } from '../hooks/use-component-preview';
 import { FormContainer } from '../ui/form-container';
+import {ResultOf} from "gql.tada";
+import {ComponentFormFieldsFragment} from "#/components/form-ctf/form-ctf";
 
 export const FormCtfClient: React.FC<{
-  data: ComponentFormFieldsFragment;
+  data: ResultOf<typeof ComponentFormFieldsFragment>;
 }> = (props) => {
   const { data: originalData } = props;
   const { data, addAttributes } =
-    useComponentPreview<ComponentFormFieldsFragment>(originalData);
+    useComponentPreview<typeof originalData>(originalData);
   if(data.form) {
     // @TODO: Fix typings for formsMap.
     // @ts-ignore
