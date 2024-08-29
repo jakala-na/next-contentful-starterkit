55a4dd91879593ebadc9b6851c5e2360dc0e7850
andreyjan <chemberdjian@gmail.com>
2024-08-29 18:03:04 +0300

    Add the possibility to disable localization.

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,9 @@ import { NextRequest } from 'next/server';
 const I18nMiddleware = createI18nMiddleware({
   locales: ['en', 'de'],
   defaultLocale: 'en',
+  // Uncomment the next setting (urlMappingStrategy) in order to use this starterkit without localization.
+  // It will open default language without locale prefix, e.g. will open  /products instead of /en/products
+  // urlMappingStrategy: 'rewriteDefault',
 });
 
 export function middleware(request: NextRequest) {
