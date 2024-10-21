f2555444f7f81dfcd758ba91136d7db087aa7475
Andrei Tipa <andrei.tipa@ffwagency.com>
2024-10-21 09:52:03 -0600

    Implement error boundaries for ComponentRenderer.

---
+++ components/error-boundary/error-boundary.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import React, { ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  errorMessage: string;
+}
+class ErrorBoundary extends React.Component<Props> {
+  public state: ErrorBoundaryState = {
+    errorMessage: '',
+  };
+  constructor(props: Props) {
+    super(props);
+  }
+
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    return { errorMessage: error.message };
+  }
+
+  render() {
+    if (this.state.errorMessage) {
+      console.log('this.state.hasError', this.state.errorMessage);
+      return (
+        <div className="mx-auto max-w-6xl px-5 py-12">
+          <div className="rounded-xl bg-gray p-8 text-center">
+            <strong>Unable to render the component.</strong>
+            <pre>{this.state.errorMessage}</pre>
+          </div>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
