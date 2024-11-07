4afd7f6d6381755922b7dcdb8e0438dcadd5afd3
Rob Pinciuc <robert.pinciuc@ffw.com>
2024-11-07 13:22:18 -0800

    chore: replace <a> with <Link> to resolve eslint @next/next/no-html-link-for-pages

--- components/site-header/site-header.tsx
+++ components/site-header/site-header.tsx
@@ -1,18 +1,19 @@
-import { draftMode } from 'next/headers';
+import { draftMode, type UnsafeUnwrappedDraftMode } from 'next/headers';
+import Link from 'next/link';
 
 import { Navigation } from '#/components/navigation';
 
 // TODO: Fix fragment unmasking to type navigationData, if possible.
 export function SiteHeader(props: { navigationData: any }) {
-  const { isEnabled: isDraftMode } = draftMode();
+  const { isEnabled: isDraftMode } = draftMode() as unknown as UnsafeUnwrappedDraftMode;
   return (
     <>
       {isDraftMode && (
         <div className="mb-4 border-b-2 border-primary bg-accent px-6 py-5 text-base text-primary" role="alert">
           You are in draft mode. As part of draft mode you can see unpublished content. To exit draft mode click{' '}
-          <a className={'underline'} href="/api/disable-draft">
+          <Link className={'underline'} href="/api/disable-draft">
             here
-          </a>
+          </Link>
           .
         </div>
       )}
