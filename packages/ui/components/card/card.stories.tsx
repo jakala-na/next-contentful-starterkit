87bf839bbb13dae316c557f89028ed7fea5a3a21
Rob Pinciuc <robert.pinciuc@ffw.com>
2025-02-20 21:38:48 -0800

    feat: add Card UI component

---
+++ packages/ui/components/card/card.stories.tsx
@@ -0,0 +1,74 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { colorConfigs } from '@repo/ui/theme';
+
+import { Card } from './card';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: 'UI/Card',
+  component: Card,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    headline: { control: 'text' },
+    bodyText: { control: 'object' },
+    image: { control: 'object' },
+    colorPalette: {
+      control: 'select',
+      options: Object.keys(colorConfigs),
+    },
+  },
+} satisfies Meta<typeof Card>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const defaultArgs = {
+  headline: 'With great power comes great responsibility',
+  bodyText: (
+    <div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, libero eget ultricies aliquam, nisl.</p>
+    </div>
+  ),
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {
+  args: {
+    ...defaultArgs,
+  },
+};
+
+export const WithImage: Story = {
+  args: {
+    ...defaultArgs,
+    image: {
+      src: 'https://picsum.photos/seed/picsum/1920/1080',
+      alt: 'Placeholder image',
+    },
+  },
+};
+
+export const WithCta: Story = {
+  args: {
+    ...defaultArgs,
+    cta: { children: 'Learn more', href: 'https://google.com' },
+  },
+};
+
+export const WithImageAndCta: Story = {
+  args: {
+    ...defaultArgs,
+    image: {
+      src: 'https://picsum.photos/seed/picsum/1920/1080',
+      alt: 'Placeholder image',
+    },
+    cta: { children: 'Learn more', href: 'https://google.com' },
+  },
+};
---
+++ packages/ui/components/card/card.tsx
@@ -0,0 +1,67 @@
+import { type ReactNode } from 'react';
+
+import { getColorConfigFromPalette } from '../../theme';
+import { Button } from '../button';
+import { Image, type ImageProps } from '../image';
+import type { LinkProps } from '../link';
+import { Link } from '../link';
+
+interface CardProps {
+  headline?: string | null;
+  bodyText?: ReactNode;
+  image?: ImageProps | null;
+  cta?: LinkProps | null;
+  colorPalette?: string | null;
+  addAttributes?: (name: string) => object | null;
+  onClickAnalyticsEvent?: () => void;
+}
+
+export function Card(props: CardProps) {
+  const {
+    headline,
+    bodyText,
+    image,
+    cta,
+    colorPalette,
+    addAttributes = () => ({}), // Default to no-op.
+    onClickAnalyticsEvent,
+  } = props;
+  const colorConfig = getColorConfigFromPalette(colorPalette ?? '');
+
+  return (
+    <div
+      className="max-w-sm rounded-2xl shadow-md overflow-hidden"
+      style={{ backgroundColor: colorConfig.backgroundColor }}
+    >
+      <div className="aspect-w-16 aspect-h-9">
+        {image ? <Image {...addAttributes('image')} {...image} alt={image.alt} className="w-full" /> : null}{' '}
+      </div>
+      <div className="p-4">
+        {headline ? (
+          <h2 className="mb-4 text-4xl font-bold" style={{ color: colorConfig.headlineColor }}>
+            {headline}
+          </h2>
+        ) : null}
+        {bodyText ? (
+          <div className="wysiwyg text-lg" style={{ color: colorConfig.textColor }}>
+            {bodyText}
+          </div>
+        ) : null}
+        {cta?.href && cta.children ? (
+          <div className="mt-6">
+            <Button
+              variant={colorConfig.buttonColor}
+              {...addAttributes('ctaText')}
+              asChild
+              onClick={() => {
+                onClickAnalyticsEvent?.();
+              }}
+            >
+              <Link {...cta} />
+            </Button>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+}
