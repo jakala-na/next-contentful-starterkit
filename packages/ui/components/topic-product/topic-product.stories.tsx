bc5e3308ba900e637825877cf251c3ff66a2ee2f
Rob Pinciuc <robert.pinciuc@ffw.com>
2025-02-21 11:27:14 -0800

    chore: move TopicProduct to Topics parent set in Storybook

--- packages/ui/components/topic-product/topic-product.stories.tsx
+++ packages/ui/components/topic-product/topic-product.stories.tsx
@@ -4,7 +4,7 @@ import { TopicProduct } from './topic-product';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: 'Components/TopicProduct',
+  title: 'Topics/Product',
   component: TopicProduct,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
