04b1e61df403198b25103cef07d3e915c7e1229b
Rob Pinciuc <robert.pinciuc@ffw.com>
2025-02-25 20:59:12 -0800

    chore: add comment to describe unmasked gql fragment

--- apps/marketing/components/taxonomy-concept-ctf/taxonomy-concept-ctf.tsx
+++ apps/marketing/components/taxonomy-concept-ctf/taxonomy-concept-ctf.tsx
@@ -1,5 +1,7 @@
 import { type ResultOf, type FragmentOf, graphql } from 'gql.tada';
 
+// This fragment must be unmasked since we are adding data to its results.
+// Masking then unmasking it will cause typescript errors.
 export const TaxonomyConceptFragment = graphql(`
   fragment TaxonomyConcept on TaxonomyConcept @_unmask {
     __typename
